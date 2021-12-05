3a1fae4b01b851b6ff65a441db4bae53a468f780
Ashvi Gupta <94855745+ashvi2021@users.noreply.github.com>
2021-12-05 18:47:37 +0530

    Add files via upload

---
+++ Day_4_Exercise_2.py
@@ -0,0 +1,12 @@
+import random
+names_string = input("Give me everybody's names, seperated by a comma.")
+names=names_string.split(", " )
+
+print(names)
+num_items = len(names)
+print(num_items)
+
+random_choice = random.randint(0, num_items - 1)
+person_who_will_pay = names[random_choice]
+print(person_who_will_pay + "is going to pay the meal today.")
+      
